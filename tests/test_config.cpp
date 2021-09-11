43446d1cf16c309da02f087c3469a763d8034be8
alfmunny <alfmunny@gmail.com>
2021-09-12 00:18:04 +0200

    Config from yaml added

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -11,11 +11,17 @@ wille::ConfigVar<float>::ptr g_float_value_config =
 void test_yaml() {
     YAML::Node root = YAML::LoadFile("/Users/alfmunny/Projects/wille/bin/conf/log.yml");
     WILLE_LOG_INFO(WILLE_LOG_ROOT()) << root;
+
+    WILLE_LOG_INFO(WILLE_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
+    WILLE_LOG_INFO(WILLE_LOG_ROOT()) << "before: " << g_float_value_config->toString();
+
+    wille::Config::LoadFromYaml(root);
+
+    WILLE_LOG_INFO(WILLE_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
+    WILLE_LOG_INFO(WILLE_LOG_ROOT()) << "after: " << g_float_value_config->toString();
 }
 
 int main(int argc, char** argv) {
-    WILLE_LOG_INFO(WILLE_LOG_ROOT()) << g_int_value_config->getValue();
-    WILLE_LOG_INFO(WILLE_LOG_ROOT()) << g_float_value_config->toString();
     test_yaml();
     return 0;
 }
